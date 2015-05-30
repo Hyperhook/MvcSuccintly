efa81bc02a3a959844279a2a39920216ec5be45e
Onilol <perronium@gmail.com>
2015-05-30 09:35:29 -0300

    First upload

---
+++ MvcSuccintly/Controllers/ItineraryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcSuccintly.Models;
+
+namespace MvcSuccintly.Controllers
+{
+    public class ItineraryController : Controller
+    {
+        //
+        // GET: /Itinerary/
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new ItineraryItem();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ItineraryItem data)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "The data is invalid");
+            }
+            return View();
+        }
+
+    }
+}
